5f61ea8f8a7210250105982636aae773edfd8e9a
olimov Amir <olimov.amir@mail.ru>
2023-12-12 09:33:56 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Нажмите любую клавишу для получение кода");
+        while (true)
+        {
+            
+            ConsoleKey key = Console.ReadKey().Key;
+
+            int keyCode = (int)key;
+
+            Console.WriteLine($"\tEnum {key}\tKey Code {keyCode}");
+
+            if (key == ConsoleKey.Enter)
+            {
+                Console.WriteLine("Вы нажали Enter");
+            }
+        }
+    }
+}
